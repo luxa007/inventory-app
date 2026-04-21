e0a2826e28f58dc8fafd713384619fbeb301fbb5
luxa007 <lucianotinotenda003@gmail.com>
2026-04-21 16:23:54 +0530

    security: upgrade to BCrypt password encoding

--- src/main/java/com/Luxa/inventory/config/AppSecurityBeans.java
+++ src/main/java/com/Luxa/inventory/config/AppSecurityBeans.java
@@ -2,13 +2,13 @@ package com.Luxa.inventory.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class AppSecurityBeans {
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 }
