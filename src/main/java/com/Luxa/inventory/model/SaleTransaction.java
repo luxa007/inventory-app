3b9526cb4a2f87234203bcd3e6a640bcab44b0a1
luxa007 <lucianotinotenda003@gmail.com>
2026-06-07 18:50:43 +0530

    fix: eager fetch product in SaleTransaction to prevent LazyInit exception

--- src/main/java/com/Luxa/inventory/model/SaleTransaction.java
+++ src/main/java/com/Luxa/inventory/model/SaleTransaction.java
@@ -12,7 +12,7 @@ public class SaleTransaction {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "product_id", nullable = false)
     private Product product;
 
