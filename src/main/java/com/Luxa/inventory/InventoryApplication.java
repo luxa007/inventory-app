aab2ca302de73cf0735764239116e99c49f6e67d
luxa007 <lucianotinotenda003@gmail.com>
2026-07-23 01:27:25 +0530

    debug: print datasource env vars before startup

--- src/main/java/com/Luxa/inventory/InventoryApplication.java
+++ src/main/java/com/Luxa/inventory/InventoryApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class InventoryApplication {
     public static void main(String[] args) {
-        SpringApplication.run(InventoryApplication.class, args);
         System.out.println("DEBUG DATASOURCE URL: " + System.getenv("SPRING_DATASOURCE_URL"));
         System.out.println("DEBUG DATASOURCE USERNAME: " + System.getenv("SPRING_DATASOURCE_USERNAME"));
+        SpringApplication.run(InventoryApplication.class, args);
     }
 }
